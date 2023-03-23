cfe41349c9af0b7c86082ccc3aad511794364b2f
Steven <digitalexile99@gmail.com>
2023-03-22 19:57:51 -0500

    added line clamping to prevent overflow

--- src/ArticleCard.jsx
+++ src/ArticleCard.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function ArticleCard({ id, author, title, body, created_at, updated_at }) {
   const date = new Date(created_at);
-  const formattedDate = date.toLocaleString("en-US", { hour12: true });
+  const formattedDate = date.toLocaleDateString("en-US");
 
   return (
-    <div className="h-52 w-72 bg-slate-300 mb-5 mt-10 m-auto ">
-      <h1 className="text-center">{title}</h1>
-      <p className="lg:w-64 truncate ...">{body}</p>
-      <div>
+    <div className="h-52 w-72 bg-slate-300 mb-5 mt-10 m-auto px-2 py-3 content-between rounded-xl">
+      <h1 className="text-center mb-5 lg:text-2xl">{title}</h1>
+      <p className="line-clamp-3 mb-8">{body}</p>
+      <div className="text-sm flex justify-around">
         <p>by: {author}</p>
-        <p>created on: {formattedDate}</p>
+        <p>created: {formattedDate}</p>
       </div>
     </div>
   );
