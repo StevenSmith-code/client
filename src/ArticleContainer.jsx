e73680476e9d762d4f5a11be09f626b67fc5b99d
Steven <digitalexile99@gmail.com>
2023-03-23 10:49:21 -0500

    added patch req functionality

--- src/ArticleContainer.jsx
+++ src/ArticleContainer.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function ArticleContainer() {
   let { id } = useParams();
+  const [isHidden, setIsHidden] = useState(true);
   const [article, setArticle] = useState([]);
+  const [updateArticle, setUpdateArticle] = useState("");
   const date = new Date(article.updated_at);
   const formattedDate = date.toLocaleString("en-US", { hour12: true });
 
@@ -13,13 +18,55 @@ function ArticleContainer() {
       .then((data) => setArticle(data));
   }, []);
 
+  function handleUpdate() {
+    if (isHidden) {
+      setUpdateArticle(article.body);
+      setIsHidden(!isHidden);
+    }
+    fetch(`http://localhost:9292/articles/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        body: updateArticle,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setArticle(data));
+    setIsHidden(!isHidden);
+  }
+
   return (
     <div className="flex flex-col bg-gray-800 h-screen">
       <div className="max-w-7xl m-auto space-y-4 z-10 bg-gray-500 text-white h-screen">
         <h1 className="text-center lg:text-4xl mt-10">{article.title}</h1>
         <h2 className="text-center lg:text-xl">Made by: {article.author}</h2>
-        <h2 className="text-center">Last Updated: {formattedDate}</h2>
-        <p className="px-10">{article.body}</p>
+        <div className="flex justify-center">
+          <h2 className="text-center mr-5">Last Updated: {formattedDate}</h2>
+          {isHidden && (
+            <EditIcon onClick={handleUpdate} className="border border-black" />
+          )}
+        </div>
+        {isHidden ? (
+          <p className="px-10">{article.body}</p>
+        ) : (
+          <TextField
+            id="outlined-multiline-static"
+            sx={{ m: 1, width: "75ch" }}
+            label="Write your blog here"
+            multiline
+            rows={35}
+            value={updateArticle}
+            onChange={(e) => setUpdateArticle(e.target.value)}
+            placeholder="Write your blog here..."
+          />
+        )}
+        {!isHidden && (
+          <Button className="w-36" onClick={handleUpdate} variant="contained">
+            Submit Changes
+          </Button>
+        )}
       </div>
     </div>
   );
